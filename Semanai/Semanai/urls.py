dd5636a8ccd9ae83ec763150e3c69d7c0d828ead
Salchiis <d1360_audi@hotmail.com>
2022-10-24 15:05:32 -0500

    Add things

--- Semanai/Semanai/urls.py
+++ Semanai/Semanai/urls.py
@@ -18,8 +18,7 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name="index" ),
+    path('',views.home,name="landingPage" ),
 ]
--- Semanai/Semanai/views.py
+++ Semanai/Semanai/views.py
@@ -5,5 +5,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("hello Audi")
+
+def home(request):
+    return render(request, 'index.html')
