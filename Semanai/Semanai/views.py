7e183b4bdc51d49d4923dcaba4ed3c86a960b60e
A01733186 <A01733186@itesm.mx>
2022-10-26 16:19:25 -0500

    Se agregaron secciones y tarjetas al modelo

--- Semanai/Semanai/views.py
+++ Semanai/Semanai/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import *
 
-from Semanai.models import miembros
+from Semanai.models import miembros, secciones
 
 def home(request):
     return render(request, 'index.html')
@@ -14,3 +14,7 @@ def home(request):
 def members(request):
     members = miembros.objects.all()
     return render(request, 'members.html',{'members': members})
+
+def specs(request):
+    specs = secciones.objects.all()
+    return render(request, 'specs.html',{'secciones': specs})
