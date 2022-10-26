a57c75be47d3e5d819a67a5e50eb670c83d42308
Salchiis <d1360_audi@hotmail.com>
2022-10-26 16:12:41 -0500

    Minor

--- Semanai/Semanai/models.py
+++ Semanai/Semanai/models.py
@@ -8,17 +8,3 @@ class miembros(models.Model):
         upload_to='static/img', null=True, blank=True)
     def __str__(self):
         return self.name
-    
-class seccion(models.Model):
-    title = models.CharField("Título",max_length = 50)
-    description = models.TextField()
-    def __str__(self):
-        return self.title
-
-class card(models.Model):
-    title = models.CharField("Título",max_length = 50)
-    description = models.TextField()
-    profilePicture = models.ImageField(
-        upload_to='static/img', null=True, blank=True)
-    def __str__(self):
-        return self.title
